982072c9f111fed6b06bdb17a27c4eb2f48328ec
Adipati Ma'ruf Alamsyah <adipati.cs.samsung@gmail.com>
2021-02-02 15:15:10 +0700

    add jsconfig.json, add Button elements

---
+++ src/elements/Button/index.js
@@ -0,0 +1,83 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+
+export default function Button(props) {
+  const className = [props.className];
+  if (props.isPrimary) className.push('btn-primary');
+  if (props.isLarge) className.push('btn-lg');
+  if (props.isSmall) className.push('btn-sm');
+  if (props.isBlock) className.push('btn-block');
+  if (props.hasShadow) className.push('btn-shadow');
+
+  const onClick = () => {
+    if (props.onClick) props.onClick();
+  };
+
+  if (props.isDisabled || props.isLoading) {
+    if (props.isDisabled) className.push('disabled');
+    return (
+      <span className={className.join(' ')} style={props.style}>
+        {props.isLoading ? (
+          <>
+            <span class="spinner-border spinner-border-sm mx-5"></span>
+            <span class="sr-only">Loading...</span>
+          </>
+        ) : (
+          props.children
+        )}
+      </span>
+    );
+  }
+
+  if (props.type === 'link') {
+    if (props.isExternal) {
+      return (
+        <a
+          href={props.href}
+          className={className.join(' ')}
+          style={props.style}
+          target={props.target === '_blank' ? '_blank' : undefined}
+          rel={props.target === '_blank' ? 'noopener noreferrer' : undefined}
+        >
+          {props.children}
+        </a>
+      );
+    } else {
+      <Link
+        to={props.href}
+        className={className.join(' ')}
+        style={props.style}
+        onClick={onClick}
+      >
+        {props.children}
+      </Link>;
+    }
+  }
+
+  return (
+    <button
+      to={props.href}
+      className={className.join(' ')}
+      style={props.style}
+      onClick={onClick}
+    >
+      {props.children}
+    </button>
+  );
+}
+
+Button.propTypes = {
+  type: PropTypes.oneOf(['button', 'link']),
+  onClick: PropTypes.func,
+  target: PropTypes.string,
+  href: PropTypes.string,
+  className: PropTypes.string,
+  isDisabled: PropTypes.bool,
+  isLoading: PropTypes.bool,
+  isSmall: PropTypes.bool,
+  isLarge: PropTypes.bool,
+  isBlock: PropTypes.bool,
+  isExternal: PropTypes.bool,
+  hasShadow: PropTypes.bool,
+};
