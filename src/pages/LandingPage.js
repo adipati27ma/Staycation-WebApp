919944fec6e1b0ee78fb1e55b057ac3a83819c61
Adipati Ma'ruf Alamsyah <adipati.cs.samsung@gmail.com>
2022-02-17 13:29:05 +0700

    [Add] display grid & simple grid markup _scss

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -3,12 +3,14 @@ import React, { Component } from 'react';
 import landingPage from 'json/landingPage.json';
 import Header from 'parts/Header';
 import Hero from 'parts/Hero';
+import MostPicked from 'parts/MostPicked';
 
 export default function LandingPage(props) {
   return (
     <>
       <Header {...props}></Header>
       <Hero data={landingPage.hero} />
+      <MostPicked data={landingPage.mostPicked} />
     </>
   );
 }
